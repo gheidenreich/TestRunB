9d6a4a0c0a1a27687ca5d3488c9762b96f1525ca
G Heidenreich <gheidenreich@gmail.com>
2016-02-18 15:04:29 -0600

    First commit ger

--- TestRunB/Startup.cs
+++ TestRunB/Startup.cs
@@ -19,7 +19,7 @@ namespace TestRunB
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Aaron!");
             });
         }
     }
